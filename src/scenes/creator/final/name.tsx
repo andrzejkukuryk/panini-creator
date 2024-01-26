fb2800f3c63a48ac87852c7228b929bdb00b7070
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2024-01-26 22:38:32 +0100

    add name

--- src/scenes/creator/final/name.tsx
+++ src/scenes/creator/final/name.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./name.module.scss";
 import { Label } from "../../../components/label";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,14 +10,22 @@ export function Name() {
 
   const paniniName = useSelector(nameSelector);
 
-  const handleChange = (name: string) => {
-    dispatch(updateName(name));
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateName(event.target.value));
   };
+
+  useEffect(() => console.log(paniniName), [paniniName]);
 
   return (
     <div className={styles.nameContainer}>
       <Label text="Panini name" />
-      <input type="text" className={styles.inputCell}></input>
+      <input
+        type="text"
+        className={styles.inputCell}
+        onChange={handleChange}
+        value={paniniName}
+        placeholder="eg. Club Panini"
+      ></input>
     </div>
   );
 }
