604fd6080e68cc38e48f79e5a19f86707c1e6aa2
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2024-02-09 20:36:06 +0100

    before new branch

--- src/scenes/orders/dropdownOrders.tsx
+++ src/scenes/orders/dropdownOrders.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./dropdowndOrders.module.scss";
 import Select, { GroupBase, SingleValue, StylesConfig } from "react-select";
 import { useDispatch, useSelector } from "react-redux";
