5b744b1c1e8d17f32bf7c59afcaa4c691e07c8a9
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2024-02-27 09:12:44 +0100

    lesson logic fixes

--- src/scenes/creator/header.tsx
+++ src/scenes/creator/header.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import styles from "./header.module.scss";
 import { Title } from "../../components/title";
 import { AppButton } from "../../components/appButton";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { randomBread } from "../../store/bread/breadSlice";
 import { AppDispatch } from "../../store/store";
 import { randomCheese } from "../../store/cheese/cheeseSlice";
 import { randomMeat } from "../../store/meat/meatSlice";
+import { randomOrderSelector } from "../../store/orders/selectors";
 
 export function Header() {
   const dispatch = useDispatch<AppDispatch>();
-
+  const randomCheeses = useSelector(randomOrderSelector);
   const handleClickRandom = () => {
-    dispatch(randomBread());
-    dispatch(randomCheese());
-    dispatch(randomMeat());
+    // dispatch(randomBread());
+    // dispatch(randomCheese());
+    // dispatch(randomMeat());
   };
 
   return (
--- src/store/orders/selectors.tsx
+++ src/store/orders/selectors.tsx
@@ -1,6 +1,6 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-import { currentSceneSelector } from "../appControl/selectors";
+import { cheeseVariantsSelector } from "../cheese/selectors";
 
 export const ordersSelector = (state: RootState) => {
   return state.orders.orders;
@@ -20,4 +20,58 @@ export const currentOrderIdSelector = (state: RootState) => {
 
 export const currentOrderSelector = (state: RootState) => {
   return state.orders.orders[state.orders.currentOrderIndex];
+};
+
+export const randomOrderSelector = createSelector(
+  [cheeseVariantsSelector],
+  (cheeses) => {
+    // const randomBoolean = () => {
+    //   if (Math.random() > 0.5) {
+    //     return false;
+    //   } else {
+    //     return true;
+    //   }
+    // };
+    // const randomIndex = (arr: string[]) => Math.floor(Math.random() * arr.length);
+
+    // const randomCheeses: string[] = [];
+    // let addCheese: boolean = true;
+    // const addNextCheese = () => {
+    //   randomCheeses.push(cheeses[randomIndex(cheeses)]);
+    //   if (randomBoolean()) {
+    //     addNextCheese();
+    //   }
+    // };
+
+    // if (randomBoolean()) {
+    //   addNextCheese();
+    // } else {
+    //   addCheese = false;
+    // }
+
+    // return {
+    //   addCheese: addCheese,
+    //   cheeses: randomCheeses,
+    // };
+    const randomCheeses = randomizeArray(cheeses, []);
+    console.log(randomCheeses);
+  }
+);
+
+const randomizeArray = (inputArr: string[], outputArr: string[]): string[] => {
+  const randomBoolean = () => {
+    if (Math.random() > 0.5) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const randomIndex = (arr: string[]) => Math.floor(Math.random() * arr.length);
+  if (randomBoolean()) {
+    outputArr.push(inputArr[randomIndex(inputArr)]);
+    return randomizeArray(inputArr, outputArr);
+  } else {
+    return outputArr;
+  }
 };
